542a749567c036a5ae666ba43cde8a5aa28d83e6
prasant46 <pdprasantapd46245@gmail.com>
2025-01-03 23:55:30 +0530

    commit

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -9,7 +9,7 @@ const login = () => {
         <form className='flex flex-col items-center justify-center'>
           <input className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full placeholder:text-white' type="email"placeholder='Enter your email' />
           <input className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full mt-3 placeholder:text-white' type="password"placeholder='Enter your password' />
-          <button className='text-white outline-none  border-2 border-none bg-emerald-600 text-xl py-4 px-5 rounded-full mt-5 placeholder:text-white'>Login</button>
+          <button  className='text-white outline-none  border-2 border-none bg-emerald-600 text-xl py-4 px-5 rounded-full mt-5  placeholder:text-white'>Login</button>
 
         </form>
       </div>
