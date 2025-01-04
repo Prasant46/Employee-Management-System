beda79f9ccac57632696dd1eb3ee0ce4ff9824f8
prasant46 <pdprasantapd46245@gmail.com>
2025-01-04 13:50:20 +0530

    test5

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const login = () => {
+const Login = ({handleLogin}) => {
 
-  
+    
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        handleLogin(email,password)
+        setEmail("")
+        setPassword("")
+        
+        
+    }
+
+
   return (
-    <div className=' flex h-screen w-screen items-center justify-center'>
-      <div className='border-2 p-20 border-emerald-600'>
-        <form className='flex flex-col items-center justify-center'>
-          <input className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full placeholder:text-white' type="email"placeholder='Enter your email' />
-          <input className='text-black outline-none bg-transparent border-2 border-emerald-600 text-xl py-4 px-5 rounded-full mt-3 placeholder:text-white' type="password"placeholder='Enter your password' />
-          <button  className='text-white outline-none  border-2 border-none bg-emerald-600 text-xl py-4 px-5 rounded-full mt-5  placeholder:text-white'>Login</button>
-
-        </form>
-      </div>
-      
+    <div className='flex h-screen w-screen items-center justify-center'>
+        <div className='border-2 rounded-xl border-emerald-600 p-20'>
+            <form 
+            onSubmit={(e)=>{
+                submitHandler(e)
+            }}
+            className='flex flex-col items-center justify-center'
+            >
+                <input 
+                value={email}
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email' 
+                />
+                <input
+                value={password}
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
+                required 
+                className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
+                <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
+            </form>
+        </div>
     </div>
   )
 }
 
-export default login
+export default Login
